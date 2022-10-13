47c2dba48995e4e00f37a237f44653c8c0f7f443
Ujjwal Aggarwal <ujj.aggarwal@gmail.com>
2022-10-13 11:45:37 +0400

    First Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by ujjy aggy on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by ujjy aggy on 13/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
